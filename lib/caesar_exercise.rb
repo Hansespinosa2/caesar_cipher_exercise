ba6a38967b0987ff8007bd859191cab47fa5e959
Hansespinosa2 <andresespinosa@ufl.edu>
2024-03-01 12:02:18 -0500

    Delete the unnecessary specs

--- lib/caesar_exercise.rb
+++ lib/caesar_exercise.rb
@@ -1,20 +1,20 @@
 def get_shift(number)
   ((number.to_f/25.to_f - number/25)*25).round
-  end
+end
 
-  def upper?(string)
-    string.upcase == string
-  end
+def upper?(string)
+  string.upcase == string
+end
 
 
-  def caesar_cipher(string = "No input", number = 0)
-    alphabet_list = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.split('')
-    arr_indices = []
-    new_arr = []
-    string.split('').each {|char| arr_indices.append(alphabet_list.find_index(char.downcase))}
-    arr_indices.map! do |index|
-      index.nil? ? index:index+get_shift(number)
-    end
-    arr_indices.each_with_index {|alpha_index, index| alpha_index.nil? ? new_arr.append(string[index]) : new_arr.append(upper?(string[index]) ? alphabet_list[alpha_index].upcase : alphabet_list[alpha_index] )}
-    puts new_arr.join()
+def caesar_cipher(string = "No input", number = 0)
+  alphabet_list = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.split('')
+  arr_indices = []
+  new_arr = []
+  string.split('').each {|char| arr_indices.append(alphabet_list.find_index(char.downcase))}
+  arr_indices.map! do |index|
+    index.nil? ? index:index+get_shift(number)
   end
+  arr_indices.each_with_index {|alpha_index, index| alpha_index.nil? ? new_arr.append(string[index]) : new_arr.append(upper?(string[index]) ? alphabet_list[alpha_index].upcase : alphabet_list[alpha_index] )}
+  puts new_arr.join()
+end
